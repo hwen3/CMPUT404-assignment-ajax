e803520f076f6e53d99bcf63fa9b72fb10a995fa
hwen3 <hwen3@ualberta.ca>
2015-02-28 17:35:39 -0700

    made the code work

--- server.py
+++ server.py
@@ -22,7 +22,7 @@
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, jsonify
 import json
 app = Flask(__name__)
 app.debug = True
@@ -57,7 +57,8 @@ class World:
 # you can test your webservice from the commandline
 # curl -v   -H "Content-Type: appication/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
 
-myWorld = World()          
+myWorld = World()
+RESPONSE_OK = 200
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -73,28 +74,28 @@ def flask_post_json():
 
 @app.route("/")
 def hello():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect('/static/index.html')
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
-    '''update the entities via this interface'''
-    return None
+    jsonData = flask_post_json()
+    if jsonData != None:
+        for prop in jsonData:
+            myWorld.update(entity, prop, jsonData[prop])
+    return jsonify(myWorld.get(entity)), RESPONSE_OK
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
-    '''you should probably return the world here'''
-    return None
+    return jsonify(myWorld.world()), RESPONSE_OK
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return jsonify(myWorld.get(entity)), RESPONSE_OK
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
-    '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return "Cleared!", RESPONSE_OK
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=9000)
